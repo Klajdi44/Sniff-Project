7699aecef2c41b7e1c164b42935ef57e8916c962
Marcelina Jankowska <jankowskam401@gmail.com>
2020-10-09 16:58:27 +0200

    sniff moving left and right with reflecting the image

--- script.js
+++ script.js
@@ -1,90 +1,163 @@
-'use strict';
+"use strict";
 
-window.addEventListener('load', start);
+window.addEventListener("load", start);
 
 //global vars
-const sniff = document.querySelector('#sniff');
+let i = 0;
+let sniff;
+let sniffLeft;
+let foxArray;
 let leftKey = false;
 let rightKey = false;
 let aKey = false;
 let dKey = false;
+let leftFoxArray;
+let rightFoxArray;
 
 const sniffObj = {
   width: 100,
   height: 100,
   x: 800,
   y: 800,
-  fill: 'orange',
-  movmentSpeed: 10
-}
+  //fill: 'orange',
+  movmentSpeed: 10,
+};
 
-
-function initialize(sprite,obj) {
-  sprite.setAttribute('x', obj.x);
-  sprite.setAttribute('y', obj.y);
-  sprite.setAttribute('fill', obj.fill);
-  sprite.setAttribute('width', obj.width);
-  sprite.setAttribute('height', obj.height);
+function initialize(sprite, obj) {
+  sprite.setAttribute("x", obj.x);
+  sprite.setAttribute("y", obj.y);
+  sprite.setAttribute("fill", obj.fill);
+  sprite.setAttribute("width", obj.width);
+  sprite.setAttribute("height", obj.height);
 }
 
 function start() {
-  console.log('ready to start');
-  initialize(sniff,sniffObj);
-  document.addEventListener('keydown', keyDown);
-  document.addEventListener('keyup', keyUp);
-  move();
-  createDrop();
+  console.log("ready to start");
+  loadSVGs();
+  //initialize(sniff,sniffObj);
+  document.addEventListener("keydown", keyDown);
+  document.addEventListener("keyup", keyUp);
+}
+
+function loadSVGs() {
+  loadSVG("sniffixed-01.svg", "#sprites", hideSnif);
+}
+
+function loadSVG(url, target, callback) {
+  fetch(url)
+    .then((response) => response.text())
+    .then((svgData) => {
+      document.querySelector(target).innerHTML = svgData;
+      if (callback) {
+        sniff = document.querySelector("#sniff");
+        leftFoxArray = document.querySelectorAll(".foxleft");
+        rightFoxArray = document.querySelectorAll(".foxright");
+        callback();
+        move();
+        createDrop();
+      }
+    });
+}
+
+function hideSnif() {
+  leftFoxArray.forEach((frame) => {
+    frame.classList.add("hidden");
+  });
+  rightFoxArray.forEach((frame) => {
+    frame.classList.add("hidden");
+  });
+  // rightFoxArray[0].classList.remove("hidden");
+
+  showFrames();
+}
+
+function showFrames() {
+  if (sniffLeft === true) {
+    foxArray = leftFoxArray;
+    rightFoxArray.forEach((frame) => {
+      frame.classList.add("hidden");
+    });
+  } else {
+    foxArray = rightFoxArray;
+    leftFoxArray.forEach((frame) => {
+      frame.classList.add("hidden");
+    });
+  }
+  if (i > -1 && i < 7) {
+    foxArray[i].classList.remove("hidden");
+    setTimeout(hideFrame, 330);
+  } else {
+    i = 0;
+    showFrames();
+  }
+}
+
+function hideFrame() {
+  if (sniffLeft === true) {
+    foxArray = leftFoxArray;
+    rightFoxArray.forEach((frame) => {
+      frame.classList.add("hidden");
+    });
+  } else {
+    foxArray = rightFoxArray;
+    leftFoxArray.forEach((frame) => {
+      frame.classList.add("hidden");
+    });
+  }
+  foxArray[i].classList.add("hidden");
+  i++;
+  showFrames();
 }
 
 function keyDown(event) {
-  if (event.key === 'ArrowLeft' || event.key === 'a') {
+  if (event.key === "ArrowLeft" || event.key === "a") {
     leftKey = true;
     aKey = true;
-  } else if (event.key === 'ArrowRight' || event.key === 'd') {
+  } else if (event.key === "ArrowRight" || event.key === "d") {
     rightKey = true;
     dKey = true;
   }
 }
 
 function keyUp(event) {
-  if (event.key === 'ArrowLeft' || event.key === 'a') {
+  if (event.key === "ArrowLeft" || event.key === "a") {
     leftKey = false;
     aKey = false;
-  } else if (event.key === 'ArrowRight' || event.key === 'd') {
+  } else if (event.key === "ArrowRight" || event.key === "d") {
     rightKey = false;
     dKey = false;
   }
 }
 
 function move() {
-  let xPosition = parseInt(sniff.getAttribute('x'));
+  let xPosition = parseInt(sniff.getAttribute("x"));
   //if  arrow keys,or A,D keys  are pressed, set them to true and limit the position so it doesnt go out of the box
-  if (rightKey) {                                                               //svg width - rectangle(sniff) width
-    sniff.setAttribute('x', Math.min(Math.max(xPosition + sniffObj.movmentSpeed), 1920 - 100));
+  if (rightKey) {
+    //svg width - rectangle(sniff) width
+    sniff.setAttribute("x", Math.min(Math.max(xPosition + sniffObj.movmentSpeed), 1920 - 100));
+    sniffLeft = false;
   } else if (leftKey) {
-    sniff.setAttribute('x', Math.min(Math.max(xPosition - sniffObj.movmentSpeed, 0)));
-
+    sniff.setAttribute("x", Math.min(Math.max(xPosition - sniffObj.movmentSpeed, 0)));
+    sniffLeft = true;
   }
   setTimeout(move, 10);
 }
 
-
-function createDrop(positionY, positionX) { 
+function createDrop(positionY, positionX) {
   // get random value between two values.   //max - min + min
   positionY = Math.floor(Math.random() * (850 - 750) + 750);
   //get random value up to 1850
-  positionX = Math.ceil((Math.random() * 1850));
+  positionX = Math.ceil(Math.random() * 1850);
   console.log(positionY, positionX);
   //create circle
-  const drop = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+  const drop = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 
   //set positionY
-  drop.setAttribute('cy', positionY);
+  drop.setAttribute("cy", positionY);
   //set position X
-  drop.setAttribute('cx', positionX);
+  drop.setAttribute("cx", positionX);
 
-
-  drop.classList.add('drop');
-  document.querySelector('#sprites').appendChild(drop);
+  drop.classList.add("drop");
+  document.querySelector("#sprites").appendChild(drop);
 }
 setInterval(createDrop, 4000);
