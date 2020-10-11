3033dadd3fdd361ae1717d5a0e32b66e6bb16aef
Gintare <gintaar3@gmail.com>
2020-10-11 14:07:28 +0200

    Menu back button

--- level-changing.js
+++ level-changing.js
@@ -2,10 +2,12 @@
 
 window.addEventListener("load", start);
 
+const mainSVG = document.querySelector("#mainSvg");
+
 function start() {
-console.log("ready to start");
-loadSVG("random.svg", "#startcontainer", createSVG, "#startSVG");
-loadSVG("anotherrandom.svg", "#levelscontainer");
+    console.log("ready to start");
+    loadSVG("random.svg", "#startcontainer", createInitSVG, "#startSVG");
+    loadSVG("anotherrandom.svg", "#levelscontainer");
 }
 
 function loadSVG(url, target,callback, createThis) {
@@ -19,36 +21,126 @@ function loadSVG(url, target,callback, createThis) {
     });
 }
 
-function createSVG(useSVG) {
-    const mainSVG = document.querySelector("#mainSvg");
-    const use = document.createElementNS("http://www.w3.org/2000/svg", "use");
-    use.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", `${useSVG}`);
-    use.setAttribute("id", "useElement");
-    mainSVG.appendChild(use);
+function createInitSVG(svg) {
+    //Check if there is used levels svg for back button event
+    if(document.querySelector("#levelsScreen")){
+        document.querySelector("#levelsScreen").remove();
+    }
 
+    useSVG(svg, `startScreen`); //create start screen
     hideContainers();
     
-    document.querySelector("#button").addEventListener("click", startButtonEvent);
+    document.querySelector("#button").addEventListener("click", startButtonEvent); //start game event
+}
+
+function useSVG(svg, useID){
+    const use = document.createElementNS("http://www.w3.org/2000/svg", "use");
+    use.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", `${svg}`);
+    use.setAttribute("id", `${useID}`);
+    document.querySelector("#background").appendChild(use);
 }
 
 function hideContainers(){
     document.querySelector("#startcontainer").classList.add("hidden");
     document.querySelector("#levelscontainer").classList.add("hidden");
-
 }
 
+//Pressing start
 function startButtonEvent(){
-    document.querySelector("#useElement").setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", `#levelsSVG`);
-    document.querySelectorAll("#levelButtons image").forEach(image=>image.addEventListener("click", loadLevelAssets))
+    fadeOutAnimation();
+
+    //Check if there is a back button already
+    removeBackButton("#startBackButton")
+
+    
+    //Timeout to play fade animation
+    setTimeout(function(){
+        //Check if there is used start svg for back button event
+        if(document.querySelector("#startScreen")){
+            document.querySelector("#startScreen").remove();
+        }
+    
+    //Replace back button
+    createBackButton("startBackButton");
+    document.querySelector("#startBackButton").addEventListener("click", goBackStart);
+
+    useSVG("#levelsSVG", "levelsScreen"); //create level selection screen
+    document.querySelectorAll("#levelButtons image").forEach(image=>image.addEventListener("click", loadLevelAssets)); //level selection events
+
+    fadeInAnimation();
+    }, 300);
 }
 
+//Load background, dropplets and other assets on level load
 function loadLevelAssets(event){
-    document.querySelector("#useElement").remove();
-    uploadBackground(`${event.target.id}.png`);
-    //also set droplet svgs to appropriate-ones
+    fadeOutAnimation();
+    const eventTargetId=event.target.id;
+    removeBackButton("#startBackButton");
+
+    setTimeout(function(){
+        //Replace back button
+        createBackButton("levelsBackButton");
+        document.querySelector("#levelsBackButton").addEventListener("click", goBackLevels);
+
+        document.querySelector("#background-image").classList.remove("hidden");
+        document.querySelector("#levelsScreen").remove();
+        uploadBackground(`${eventTargetId}.png`);
+        fadeInAnimation();
+    },300);
+    
+    //also set droplet svgs to appropriate-ones and load both snif and droplets
 }
 
 function uploadBackground(backgroundImage) {
     const bgImage = document.querySelector("#background-image");
     bgImage.setAttribute("xlink:href", `${backgroundImage}`);
+}
+
+//Create and remove back button
+function createBackButton(buttonID){
+    const backButton = document.createElementNS("http://www.w3.org/2000/svg", 'path');
+    backButton.setAttribute("d","M5,40 80,80 80,5 z");
+    backButton.setAttribute("fill","#A66844");
+    backButton.setAttribute("id",`${buttonID}`);
+    document.querySelector("#foreground").appendChild(backButton);
+}
+
+function removeBackButton(buttonID){ //Need to remove button so there is no overlaping
+    if(document.querySelector(`${buttonID}`)){
+        document.querySelector(`${buttonID}`).remove();
+    }
+}
+
+//Back button events
+function goBackLevels(event){
+    fadeOutAnimation();
+    startButtonEvent();
+
+    setTimeout(function(){
+        document.querySelector("#background-image").classList.add("hidden");
+        event.target.remove();
+        fadeInAnimation();
+    },300)
+}
+
+function goBackStart(event){
+    fadeOutAnimation();
+
+    setTimeout(function(){
+        createInitSVG("#startSVG");
+        event.target.remove();
+        fadeInAnimation();
+    },300);
+    
+}
+
+//Fade animations
+function fadeInAnimation(){
+    mainSVG.classList.remove("fadeOut");
+    mainSVG.classList.add("fadeIn");
+}
+
+function fadeOutAnimation(){
+    mainSVG.classList.remove("fadeIn");
+    mainSVG.classList.add("fadeOut");
 }
