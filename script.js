0ebbad09ccaeb16133f15cc5cbccd8f81a6c7aa4
Klajdi  Ajdini <44365608+Klajdi44@users.noreply.github.com>
2020-10-15 15:04:20 +0200

    added info circle

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-'use strict';
+"use strict";
 
-window.addEventListener('load', start);
+window.addEventListener("load", start);
 
 let timer = 3600000;
 //global vars
+let n = 2; //for bg change
 let i = 0;
 let sniff;
 let sniffLeft;
@@ -18,12 +19,15 @@ let points = 0;
 let currentDrop;
 let leftFoxArray;
 let rightFoxArray;
+let pointsHundreds = 0;
 let audio;
 let isPlaying = false;
 let walkAudio;
 let walkIsPlaying = false;
 let flying;
 let flyingIsPlaying = false;
+let info;
+let infoIsPlaying = false;
 
 const gameSounds = {
   backgroundTheme: function () {
@@ -72,7 +76,7 @@ const gameSounds = {
     catching.play();
     catching.volume = .5
   },
-  flying: function (){
+  flying: function () {
     if (typeof flying === 'undefined') {
       flying = new Audio('./gameSounds/flying.mp3');
 
@@ -86,6 +90,19 @@ const gameSounds = {
     flying.volume = .05;
     flyingIsPlaying = !flyingIsPlaying;
 
+  },
+  info: function () {
+    if (typeof info === 'undefined') {
+      info = new Audio('./gameSounds/questionMark.mp3');
+    }
+
+    if (infoIsPlaying) {
+      info.play();
+    } else {
+      info.play();
+    }
+    info.volume = .5;
+    infoIsPlaying = !infoIsPlaying;
   }
 }
 
@@ -93,28 +110,33 @@ const sniffObj = {
   width: 350,
   height: 300,
   x: 800,
-  y: 500,
+  y: 400,
   // fill: 'orange',
-  movmentSpeed: 10
-}
+  movmentSpeed: 10,
+};
 
-const arrOfDrops = [{
-  height: 100,
-  width: 100,
-  fill: 'red',
-  y: 0,
-  x: 0,
-  node: "",
-}
-]
+const arrOfDrops = [
+  {
+    height: 100,
+    width: 100,
+    fill: "red",
+    y: 0,
+    x: 0,
+    node: "",
+  },
+];
 const dropObj = arrOfDrops[0];
 
 const mainSVG = document.querySelector("#mainSvg");
 
 function start() {
+  console.log(Math.floor(localStorage.getItem("highScore") / 100))
   console.log("ready to start");
-  loadSVG("random.svg", "#startcontainer", createInitSVG, "#startSVG");
-  loadSVG("anotherrandom.svg", "#levelscontainer");
+  loadSVG("assets/random.svg", "#startcontainer", createInitSVG, "#startSVG");
+  loadSVG("assets/anotherrandom.svg", "#levelscontainer");
+  document.querySelector('#questionMark').addEventListener('click', () => {
+   gameSounds.info();
+  });
 }
 
 function loadSVG(url, target, callback, createThis) {
@@ -149,6 +171,7 @@ function useSVG(svg, useID, endPoint) {
 
 function hideContainers() {
   document.querySelector("#startcontainer").classList.add("hidden");
+  document.querySelector('#questionMark').classList.remove('hidden');
   document.querySelector("#levelscontainer").classList.add("hidden");
 }
 
@@ -157,8 +180,7 @@ function startButtonEvent() {
   fadeOutAnimation();
 
   //Check if there is a back button already
-  removeBackButton("#startBackButton")
-
+  removeBackButton("#startBackButton");
 
   //Timeout to play fade animation
   setTimeout(function () {
@@ -172,13 +194,13 @@ function startButtonEvent() {
     document.querySelector("#startBackButton").addEventListener("click", goBackStart);
 
     useSVG("#levelsSVG", "levelsScreen", "#background"); //create level selection screen
-    document.querySelectorAll("#levelButtons image").forEach(image => image.addEventListener("click", loadLevelAssets)); //level selection events
+    document.querySelectorAll("#levelButtons image").forEach((image) => image.addEventListener("click", loadLevelAssets)); //level selection events
 
     fadeInAnimation();
   }, 300);
 
+  document.querySelector('#questionMark').classList.add('hidden');
   gameSounds.backgroundTheme();
-
 }
 
 //Load background, dropplets and other assets on level load
@@ -209,7 +231,6 @@ function loadLevelAssets(event) {
     } else {
       loadFox(); //load in the fox
     }
-
   }, 300);
 
   //also set droplet svgs to appropriate-ones and load both snif and droplets
@@ -217,19 +238,22 @@ function loadLevelAssets(event) {
 
 function uploadBackground(backgroundImage) {
   const bgImage = document.querySelector("#background-image");
-  bgImage.setAttribute("xlink:href", `${backgroundImage}`);
+  bgImage.setAttribute("xlink:href", `assets/${backgroundImage}`);
+
+  waterAnimation();
 }
 
 //Create and remove back button
 function createBackButton(buttonID) {
-  const backButton = document.createElementNS("http://www.w3.org/2000/svg", 'path');
+  const backButton = document.createElementNS("http://www.w3.org/2000/svg", "path");
   backButton.setAttribute("d", "M5,40 80,80 80,5 z");
   backButton.setAttribute("fill", "#A66844");
   backButton.setAttribute("id", `${buttonID}`);
   document.querySelector("#foreground").appendChild(backButton);
 }
 
-function removeBackButton(buttonID) { //Need to remove button so there is no overlaping
+function removeBackButton(buttonID) {
+  //Need to remove button so there is no overlaping
   if (document.querySelector(`${buttonID}`)) {
     document.querySelector(`${buttonID}`).remove();
   }
@@ -247,10 +271,11 @@ function goBackLevels(event) {
     document.querySelector(".points").classList.add("hidden");
     document.querySelector(".highPoints").classList.add("hidden");
     event.target.remove();
-    document.querySelector("#sprites").innerHTML = '';
+    document.querySelector("#sprites").innerHTML = "";
+    n = 2; //reset
 
     fadeInAnimation();
-  }, 300)
+  }, 300);
 }
 
 function goBackStart(event) {
@@ -261,7 +286,6 @@ function goBackStart(event) {
     event.target.remove();
     fadeInAnimation();
   }, 300);
-
 }
 
 //Fade animations
@@ -274,28 +298,45 @@ function fadeOutAnimation() {
   mainSVG.classList.remove("fadeIn");
   mainSVG.classList.add("fadeOut");
 }
+//
 
+//Level animations
+
+function determineLevelAnimation() {
+  const backgroundImage = document.querySelector("#background-image").getAttribute("xlink:href");
+
+  if (backgroundImage.includes("water-bg")) {
+    waterAnimation();
+  }
+}
+
+function waterAnimation() {
+  let waterTear = document.querySelector(`#tear`).cloneNode(true);
+  document.querySelector("#background").appendChild(waterTear);
+  waterTear.classList.add("dropAnimation");
+}
+//
 
 function loadFox() {
-  loadSnif("sniffixed-01.svg", "#snifcontainer", hideSnif);
+  loadSnif("assets/snifhats2-01.svg", "#snifcontainer", hideSnif);
   loadDrops();
 }
 
 function loadDrops() {
   if (!document.querySelector(".waterContainer svg")) {
-    loadSVG("water.svg", ".waterContainer");
+    loadSVG("assets/water.svg", ".waterContainer");
   }
 
   if (!document.querySelector(".electricityContainer svg")) {
-    loadSVG("electricity.svg", ".electricityContainer");
+    loadSVG("assets/electricity.svg", ".electricityContainer");
   }
 
   if (!document.querySelector(".heatContainer svg")) {
-    loadSVG("heat.svg", ".heatContainer");
+    loadSVG("assets/heat.svg", ".heatContainer");
   }
 
   if (!document.querySelector(".lightContainer svg")) {
-    loadSVG("light.svg", ".lightContainer");
+    loadSVG("assets/light.svg", ".lightContainer");
   }
 }
 
@@ -312,6 +353,7 @@ function loadSnif(url, target, callback) {
         rightFoxArray = document.querySelectorAll(".foxright");
         startSnif();
         loop();
+        getHats();
         callback();
       }
     });
@@ -337,7 +379,6 @@ function hideSnif() {
 
 function showFrames() {
   if (sniffLeft === true) {
-
     foxArray = leftFoxArray;
     rightFoxArray.forEach((frame) => {
       frame.classList.add("hidden");
@@ -374,30 +415,47 @@ function hideFrame() {
   showFrames();
 }
 
+function getHats() {
+  document.querySelectorAll(".hat").forEach((hat) => {
+    hat.classList.add("hidden");
+    const hatId = hat.getAttribute("id");
+    let pointString = pointsHundreds.toString();
+
+
+    if (localStorage.getItem("highScore") >= pointString && hatId.includes(`hat${Math.floor(localStorage.getItem("highScore") / 100)}`)) {
+      hat.classList.remove("hidden");
+    }
+    else if (pointString.includes("00") && pointString > localStorage.getItem("highScore") && hatId.includes(`hat${pointsHundreds / 100}`)) {
+      hat.classList.remove("hidden");
+    } else {
+      hat.classList.add("hidden");
+    }
+  });
+}
+
 function startSnif() {
-  console.log('ready to move');
-  document.querySelector('.highScoreBtn').addEventListener('click', saveToLocalStorage);
-  document.addEventListener('keydown', keyDown);
-  document.addEventListener('keyup', keyUp);
-  window.addEventListener('resize', () => {
+  console.log("ready to move");
+  document.querySelector(".highScoreBtn").addEventListener("click", saveToLocalStorage);
+  document.addEventListener("keydown", keyDown);
+  document.addEventListener("keyup", keyUp);
+  window.addEventListener("resize", () => {
     mobileOrDesktop();
-  })
+  });
   mobileOrDesktop();
   loop();
 }
 
 function mobileOrDesktop() {
   if (window.matchMedia("(max-width: 1024px)").matches) {
-    document.querySelector('#arrowLeft').addEventListener('touchstart', touchStart);
-    document.querySelector('#arrowRight').addEventListener('touchstart', touchStart);
-    document.querySelector('#arrowLeft').addEventListener('touchend', touchEnd);
-    document.querySelector('#arrowRight').addEventListener('touchend', touchEnd);
+    document.querySelector("#arrowLeft").addEventListener("touchstart", touchStart);
+    document.querySelector("#arrowRight").addEventListener("touchstart", touchStart);
+    document.querySelector("#arrowLeft").addEventListener("touchend", touchEnd);
+    document.querySelector("#arrowRight").addEventListener("touchend", touchEnd);
   } else {
-    document.querySelector('#arrowLeft').addEventListener('mousedown', touchStart);
-    document.querySelector('#arrowRight').addEventListener('mousedown', touchStart);
-    document.querySelector('#arrowLeft').addEventListener('mouseup', touchEnd);
-    document.querySelector('#arrowRight').addEventListener('mouseup', touchEnd);
-
+    document.querySelector("#arrowLeft").addEventListener("mousedown", touchStart);
+    document.querySelector("#arrowRight").addEventListener("mousedown", touchStart);
+    document.querySelector("#arrowLeft").addEventListener("mouseup", touchEnd);
+    document.querySelector("#arrowRight").addEventListener("mouseup", touchEnd);
   }
 }
 
@@ -409,20 +467,17 @@ function loop() {
   if (timer % 200 === 0 && document.querySelector("#snifFox")) {
     // createDrop();
     createDrop();
-    localStorage.setItem('score', points);
-
-
+    localStorage.setItem("score", points);
   }
 
   draw();
 
-  arrOfDrops.forEach(drop => {
-    // console.log(drop)
+  arrOfDrops.forEach((drop) => {
     detectCollision(sniffObj, drop);
   });
 
   //set the highScore on click of the button
-  document.querySelector('.Highscore').textContent = localStorage.getItem('highScore');
+  document.querySelector(".Highscore").textContent = localStorage.getItem("highScore");
 
   requestAnimationFrame(loop);
 }
@@ -430,7 +485,8 @@ function loop() {
 function move() {
   // let xPosition = parseInt(sniff.getAttribute('x'));
   //if  arrow keys,or A,D keys  are pressed, set them to true and limit the position so it doesnt go out of the box
-  if (rightKey || rightArrowTouch) {                                                               //svg width - rectangle
+  if (rightKey || rightArrowTouch) {
+    //svg width - rectangle
     sniffObj.x = Math.min(Math.max(sniffObj.x + sniffObj.movmentSpeed), 1920 - 100);
   } else if (leftKey || leftArrowTouch) {
     sniffObj.x = Math.min(Math.max(sniffObj.x - sniffObj.movmentSpeed, 0));
@@ -438,65 +494,62 @@ function move() {
 }
 
 function draw() {
-  sniff.setAttribute('x', sniffObj.x);
-  sniff.setAttribute('y', "150");
-  sniff.setAttribute('fill', sniffObj.fill);
-  sniff.setAttribute('width', sniffObj.width);
-  sniff.setAttribute('height', sniffObj.height);
+  sniff.setAttribute("x", sniffObj.x);
+  sniff.setAttribute("y", sniffObj.y);
+  sniff.setAttribute("fill", sniffObj.fill);
+  sniff.setAttribute("width", sniffObj.width);
+  sniff.setAttribute("height", sniffObj.height);
 }
 
 function drawDrop(drop) {
   drop = createDrop();
-  drop.setAttribute('y', dropObj.y);
+  drop.setAttribute("y", dropObj.y);
   //set position X
-  drop.setAttribute('x', dropObj.x);
-  drop.setAttribute('height', dropObj.height);
-  drop.setAttribute('width', dropObj.width);
-  drop.setAttribute('fill', dropObj.fill);
+  drop.setAttribute("x", dropObj.x);
+  drop.setAttribute("height", dropObj.height);
+  drop.setAttribute("width", dropObj.width);
+  drop.setAttribute("fill", dropObj.fill);
 }
 
 function keyDown(event) {
-  if (event.key === 'ArrowLeft' || event.key === 'a') {
+  if (event.key === "ArrowLeft" || event.key === "a") {
     sniffLeft = true;
     leftKey = true;
     aKey = true;
-    gameSounds.walk();
-  } else if (event.key === 'ArrowRight' || event.key === 'd') {
+  } else if (event.key === "ArrowRight" || event.key === "d") {
     sniffLeft = false;
     rightKey = true;
     dKey = true;
-    gameSounds.walk();
-
   }
+  gameSounds.walk()
+
 }
 
 function keyUp(event) {
-  if (event.key === 'ArrowLeft' || event.key === 'a') {
+  if (event.key === "ArrowLeft" || event.key === "a") {
     leftKey = false;
     aKey = false;
-  } else if (event.key === 'ArrowRight' || event.key === 'd') {
+  } else if (event.key === "ArrowRight" || event.key === "d") {
     rightKey = false;
     dKey = false;
   }
 }
 
 function touchStart(event) {
-  if (event.target === document.querySelector('#arrowRight')) {
+  if (event.target === document.querySelector("#arrowRight")) {
     sniffLeft = false;
     rightArrowTouch = true;
-
-  } else if (event.target === document.querySelector('#arrowLeft')) {
+  } else if (event.target === document.querySelector("#arrowLeft")) {
     sniffLeft = true;
     leftArrowTouch = true;
-
   }
-
+  gameSounds.walk();
 }
 
 function touchEnd(event) {
-  if (event.target === document.querySelector('#arrowRight')) {
+  if (event.target === document.querySelector("#arrowRight")) {
     rightArrowTouch = false;
-  } else if (event.target === document.querySelector('#arrowLeft')) {
+  } else if (event.target === document.querySelector("#arrowLeft")) {
     leftArrowTouch = false;
   }
 }
@@ -505,10 +558,8 @@ function createDrop(positionX, positionY) {
   // get random value between two values.   //max - min + min
   positionY = Math.floor(Math.random() * (550 - 450) + 450);
   //get random value up to 1850
-  positionX = Math.ceil((Math.random() * 1850));
+  positionX = Math.ceil(Math.random() * 1850);
   // console.log(positionY, positionX);
- 
-
   determineDrop();
 
   let newDropObj = Object.create(dropObj);
@@ -517,14 +568,14 @@ function createDrop(positionX, positionY) {
   newDropObj.node = currentDrop;
   arrOfDrops.push(newDropObj);
 
-  currentDrop.setAttribute('y', newDropObj.y);
+  currentDrop.setAttribute("y", newDropObj.y);
   //set position X
-  currentDrop.setAttribute('x', newDropObj.x);
-  currentDrop.setAttribute('height', newDropObj.height);
-  currentDrop.setAttribute('width', newDropObj.width);
-  currentDrop.setAttribute('fill', newDropObj.fill);
+  currentDrop.setAttribute("x", newDropObj.x);
+  currentDrop.setAttribute("height", newDropObj.height);
+  currentDrop.setAttribute("width", newDropObj.width);
+  currentDrop.setAttribute("fill", newDropObj.fill);
 
-  document.querySelector('#sprites').appendChild(currentDrop);
+  document.querySelector("#sprites").appendChild(currentDrop);
   newDropObj.node.style.display = "block";
   gameSounds.flying();
   return currentDrop;
@@ -532,14 +583,15 @@ function createDrop(positionX, positionY) {
 
 function determineDrop() {
   let dropId;
-  const backgroundImage = document.querySelector("#background-image").href.baseVal;
-  if (backgroundImage == "light-bg.png") {
+  const backgroundImage = document.querySelector("#background-image").getAttribute("xlink:href");
+
+  if (backgroundImage.includes("light-bg")) {
     dropId = "#light";
-  } else if (backgroundImage == "water-bg.png") {
+  } else if (backgroundImage.includes("water-bg")) {
     dropId = "#water";
-  } else if (backgroundImage == "power-bg.png") {
+  } else if (backgroundImage.includes("power-bg")) {
     dropId = "#power";
-  } else if (backgroundImage == "heat-bg.png") {
+  } else if (backgroundImage.includes("heat-bg")) {
     dropId = "#heat";
   }
 
@@ -548,33 +600,56 @@ function determineDrop() {
 
 function setDropUse(dropId) {
   currentDrop = document.querySelector(`${dropId}`);
-  currentDrop.setAttribute('use', `href=${dropId}`);
+  currentDrop.setAttribute("use", `href=${dropId}`);
 }
 
 function detectCollision(obj1, obj2) {
-  if (obj1.x < obj2.x + obj2.width &&
-    obj1.x + obj1.width > obj2.x &&
-    obj1.y < obj2.y + obj2.height &&
-    obj1.y + obj1.height > obj2.y) {
-    console.log('collision');
+  if (obj1.x < obj2.x + obj2.width && obj1.x + obj1.width > obj2.x && obj1.y < obj2.y + obj2.height && obj1.y + obj1.height > obj2.y) {
+    console.log("collision");
     arrOfDrops.splice(obj2.node);
     modifyScore();
     obj2.node.style.display = "none";
     gameSounds.catching();
   }
-  // console.log(obj1.x, obj2.x)
+}
+
+function changeBackground() {
+  const currentImage = document.querySelector("#background-image").getAttribute("xlink:href");
+  let newImage = currentImage.substring(7, currentImage.length - 5);
+  console.log(newImage);
+
+  if (n == 2) {
+    newImage += `g${n}.png`;
+  } else if (n <= 8) {
+    newImage += `${n}.png`;
+  } else {
+    newImage = currentImage.substring(7);
+  }
+
+  n++;
+  uploadBackground(newImage);
+}
+
+function uploadBackground(backgroundImage) {
+  const bgImage = document.querySelector("#background-image");
+  bgImage.setAttribute("xlink:href", `assets/${backgroundImage}`);
 }
 
 function modifyScore() {
-  document.querySelector('.score').textContent = points += 10;
+  document.querySelector(".score").textContent = points += 10;
+  changeBackground();
+  if (points.toString().includes("00")) {
+    pointsHundreds = points;
+  }
+  getHats();
 }
 
 function saveToLocalStorage() {
-  if (parseInt(localStorage.getItem('highScore')) > points) {
-    if (confirm('The current high score is bigger than the one you want to save, do you want to overwrite?')) {
-      localStorage.setItem('highScore', points);
+  if (parseInt(localStorage.getItem("highScore")) > points) {
+    if (confirm("The current high score is bigger than the one you want to save, do you want to overwrite?")) {
+      localStorage.setItem("highScore", points);
     }
   } else {
-    localStorage.setItem('highScore', points);
+    localStorage.setItem("highScore", points);
   }
 }
